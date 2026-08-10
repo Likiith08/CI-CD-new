2fabb32e64cc1d2f351e8066398c07002925a031
Likiith08 <likithram0820@gmail.com>
2026-08-10 14:17:12 +0530

    test fails

--- app.test.js
+++ app.test.js
@@ -9,7 +9,7 @@ describe("GET /hello", () => {
  
         expect(response.statusCode).toBe(200);
  
-        expect(response.body.message).toBe("Hello World BHOO");
+        expect(response.body.message).toBe("Hello World");
     });
  
 });
